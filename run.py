a477c12af84a753109df87ad5e186c1b319fd58d
李书博 <lishubo@lishubodeMacBook-Pro.local>
2018-01-12 16:53:36 +0800

    update

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ if __name__ == "__main__":
     parse.add_argument("-d", "--debug", action="store_true", dest="debug_mode", default=False)
     parse.add_argument("-p", "--port", dest="port", default=5000)
     cmd_args = parse.parse_args()
-    app_options = {"port": int(cmd_args.port)}
-    app_options = {"host": "0.0.0.0"}
+    app_options = {"port": int(cmd_args.port), "host": "0.0.0.0"}
     if cmd_args.debug_mode:
         app_options["debug"] = True
         app_options["use_debugger"] = False
